5a8421aec3f7ae6f837bc5d2290e210a430d3032
rajib242 <rs2423897@gmail.com>
2023-07-19 02:13:04 +0530

    letflix first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from './Card'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+<>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <h1 className="heading_style"> Letflix Top Movies List </h1>
+
+  <Card 
+    imgsrc="https://wallpapercave.com/dwp1x/wp12315791.jpg"
+    title="A Netflix Original Series"
+    sname="Tu Jhoothi Main Makkaar"
+    link="https://www.netflix.com/in/title/81382254?source=35"
+  />
+  <Card
+      imgsrc="https://wallpapercave.com/dwp1x/wp11866960.jpg"
+      title="A Netflix Original Series"
+      sname="Mission Majnu"
+      link="https://ww16.watchmovierulz.to/mission-majnu-2023-hindi/full-movie-watch-online-free-5832.html"
+
+  />
+  <Card
+      imgsrc="https://wallpapercave.com/dwp1x/wp8328157.jpg"
+      title="A Netflix Original Series"
+      sname="RRR"
+      link="https://www.netflix.com/in/title/81476453?source=35"
+  />
+
+  <Card
+      imgsrc="https://wallpapercave.com/dwp1x/wp6188503.jpg"
+      title="A Netflix Original Series"
+      sname="Extraaction"
+      link="https://www.netflix.com/in/title/80230399?source=35"
+  />
+
+</>  
+  ,document.getElementById("root"));
+
